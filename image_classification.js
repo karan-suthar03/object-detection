810c6059e3cbe01b70684966337aaa61168cd044
karan-suthar03 <116728069+karan-suthar03@users.noreply.github.com>
2023-07-10 18:20:25 +0530

    Add files via upload

---
+++ image_classification.js
@@ -0,0 +1,45 @@
+//let img;
+let v = 0;
+let video;
+let detections;
+let objectDetector;
+function preload() {
+  objectDetector = ml5.objectDetector("cocossd", lol);
+}
+function lol() {
+  console.log("loaded");
+}
+function setup() {
+  background(0);
+  video = createCapture(VIDEO, videoLoaded);
+  video.hide();
+}
+function videoLoaded() {
+  console.log("video loaded");
+  createCanvas(video.width, video.height);
+  objectDetector.detect(video, detected);
+}
+function draw() {
+  image(video, 0, 0);
+  if (detections) {
+    for (let index = 0; index < detections.length; index++) {
+      const element = detections[index];
+      stroke(0, 255, 0);
+      strokeWeight(2);
+      noFill();
+      rect(element.x, element.y, element.width, element.height);
+      noStroke();
+      fill(0, 255, 0);
+      textSize(24);
+      text(element.label, element.x + 10, element.y + 24);
+    }
+    objectDetector.detect(video, detected);
+  }
+}
+
+// When the model is loaded
+function detected(error, results) {
+  detections = results;
+  //objectDetector.detect(video, detected);
+}
+// Detect objects in the video element
